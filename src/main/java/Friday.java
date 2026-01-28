f1b13bd1d6d738245c26690b4a7dbd24402923d3
sushmiithaa <rsushmiithaa@gmail.com>
2026-01-28 18:53:07 +0800

    Echo user input until 'bye' command

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+
 public class Friday {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Friday");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
-
+        String line = "\t____________________________________________________________";
+        System.out.println(line +"\n\t Hello! I'm Friday\n\t What can I do for you?\n" + line + "\n");
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+        while (! command.equals("bye")) {
+            System.out.println(line + "\n\t" + command + "\n" + line);
+            System.out.println();
+            command = in.nextLine();
+        }
+        System.out.println(line + "\n\tBye. Hope to see you again soon!\n" + line);
     }
 }
