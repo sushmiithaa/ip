8177c99927c0a683d467b42a5cc5ebdc46bf583f
sushmiithaa <rsushmiithaa@gmail.com>
2026-01-29 22:17:30 +0800

    Add mark and unmark tasks

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -5,23 +5,32 @@ public class Friday {
     public static void main(String[] args) {
         String line = "\t____________________________________________________________";
         System.out.println(line +"\n\t Hello! I'm Friday\n\t What can I do for you?\n" + line + "\n");
-        String[] inputs = new String[100];
+        Task[] tasks = new Task[100];
         int commandCount = 0;
         Scanner in = new Scanner(System.in);
         String command = in.nextLine();
         while (! command.equals("bye")) {
             System.out.println(line);
             if (command.equals("list")){
+                System.out.println("\tHere are the tasks in your list:");
                 for (int i = 0;i < commandCount;i++){
-                    System.out.println("\t" +(i+1) + ". " + inputs[i]);
+                    System.out.println("\t" +(i+1) + ". [" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
-            }
-            else if (command.isBlank()){
-                System.out.println("\tNothing has been entered");
-            }
-            else{
+            } else if (command.substring(0,4).equals("mark") || command.substring(0,6).equals("unmark")) {
+                int taskIndex = Integer.parseInt(command.split(" ")[1]);
+                if (command.substring(0, 4).equals("mark")) {
+                    tasks[taskIndex - 1].markAsDone();
+                    System.out.println("\tNice! I've marked this task as done:");
+                } else {
+                    tasks[taskIndex - 1].setDone(false);
+                    System.out.println("\tOK, I've marked this task as not done yet:");
+                }
+                System.out.println("\t" + "  [" + tasks[taskIndex - 1].getStatusIcon() + "] " + tasks[taskIndex - 1].getDescription());
+
+            } else {
+                Task t = new Task(command);
+                tasks[commandCount] = t;
                 System.out.println("\t added: " + command);
-                inputs[commandCount] = command;
                 commandCount++;
             }
             System.out.println(line+ "\n");
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone(){
+        this.isDone = true;
+    }
+}
